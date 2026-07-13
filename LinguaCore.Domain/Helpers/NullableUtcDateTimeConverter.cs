43e1b09b75ee01ce3abf254c9d7d9da464f317d3
Ahmed Tarek Saad <138727822+AhmedTSF@users.noreply.github.com>
2026-07-13 17:03:25 +0300

    handle datetime

---
+++ LinguaCore.Domain/Helpers/NullableUtcDateTimeConverter.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinguaCore.Domain.Helpers
+{
+    public class NullableUtcDateTimeConverter : ValueConverter<DateTime?, DateTime?>
+    {
+        public NullableUtcDateTimeConverter() : base(
+            v => v.HasValue ? (v.Value.Kind == DateTimeKind.Utc ? v.Value : NormalizeToUtc(v.Value)) : v,
+            v => v.HasValue ? DateTime.SpecifyKind(v.Value, DateTimeKind.Utc) : v)
+        {
+        }
+
+        private static DateTime NormalizeToUtc(DateTime value) => value.Kind switch
+        {
+            DateTimeKind.Local => value.ToUniversalTime(),
+            DateTimeKind.Unspecified => DateTime.SpecifyKind(value, DateTimeKind.Utc),
+            _ => value
+        };
+    }
+}
---
+++ LinguaCore.Domain/Helpers/UtcDateTimeConverter.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinguaCore.Domain.Helpers
+{
+    public class UtcDateTimeConverter : ValueConverter<DateTime, DateTime>
+    {
+        public UtcDateTimeConverter() : base(
+            v => v.Kind == DateTimeKind.Utc ? v : NormalizeToUtc(v),
+            v => DateTime.SpecifyKind(v, DateTimeKind.Utc))
+        {
+        }
+
+        private static DateTime NormalizeToUtc(DateTime value) => value.Kind switch
+        {
+            DateTimeKind.Local => value.ToUniversalTime(),
+            DateTimeKind.Unspecified => DateTime.SpecifyKind(value, DateTimeKind.Utc),
+            _ => value
+        };
+    }
+
+
+}
